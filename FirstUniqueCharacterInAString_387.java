3d11f04c9f6be9aa8f4bf836f03470b14eb52102
ChenSongJian <35749789+ChenSongJian@users.noreply.github.com>
2022-10-01 02:36:51 +0800

    Create FirstUniqueCharacterInAString_387.java

---
+++ FirstUniqueCharacterInAString_387.java
@@ -0,0 +1,16 @@
+// 387. First Unique Character in a String
+
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] charCount = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            charCount[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (charCount[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
