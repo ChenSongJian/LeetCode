e3fb87201eaaa0e68f4b1f3ba4879b73927742e0
ChenSongJian <35749789+ChenSongJian@users.noreply.github.com>
2022-10-04 10:41:18 +0800

    Create SymmetricTree_101.java

---
+++ SymmetricTree_101.java
@@ -0,0 +1,35 @@
+// 101. Symmetric Tree
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return testSymmetric(root.left, root.right);
+    }
+    
+    public boolean testSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        } else if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return testSymmetric(left.left, right.right) && testSymmetric(left.right, right.left);
+    }
+    
+}
