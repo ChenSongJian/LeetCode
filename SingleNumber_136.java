32d1f3b2213449412bcfcbfba4a515efaab9d468
ChenSongJian <35749789+ChenSongJian@users.noreply.github.com>
2022-10-05 01:29:20 +0800

    Create SingleNumber_136.java

---
+++ SingleNumber_136.java
@@ -0,0 +1,18 @@
+// 136. Single Number
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                set.remove(nums[i]);
+            } else {
+                set.add(nums[i]);
+            }
+        }
+        for (int num: set) {
+            return num;
+        }
+        return 0; // should not go to here;
+    }
+}
